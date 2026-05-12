6f87367476c9830eb4ea3d64c6699714f71c8002
Nicolas <nicolasmartinezmoragues@gmail.com>
2026-05-12 10:04:39 +0200

    bug fix

--- obj/Debug/net8.0/ServidorPrueba.AssemblyInfo.cs
+++ obj/Debug/net8.0/ServidorPrueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServidorPrueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f00e3a61cfa72825b8d3e938ba6827e07d21ea3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0e531f7e60fa2a273397f3eeb3e7a09b110dd5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServidorPrueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServidorPrueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
