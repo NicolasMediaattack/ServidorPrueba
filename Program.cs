09045adbc012ecc3a3d321399c9a7307f41098e2
Nicolas <nicolasmartinezmoragues@gmail.com>
2026-06-03 10:46:14 +0200

    concat video

--- Program.cs
+++ Program.cs
@@ -154,6 +154,37 @@ app.MapPost("/mensaje", async (HttpRequest request) =>
 
             Console.WriteLine(
                 $"🎬 Resultado final: {mergedVideo}");
+
+            // Copiar a carpeta pública
+
+            string mergedVideoName =
+                $"merged_{Guid.NewGuid()}.mp4";
+
+            string publicMergedVideoPath =
+                Path.Combine(
+                    publicVideosPath,
+                    mergedVideoName);
+
+            File.Copy(
+                mergedVideo,
+                publicMergedVideoPath,
+                true);
+
+            string mergedVideoUrl =
+                $"https://{request.Host}/videos/{mergedVideoName}";
+
+            Console.ForegroundColor =
+                ConsoleColor.Green;
+
+            Console.WriteLine(
+                $"🌍 MERGED VIDEO URL: {mergedVideoUrl}");
+
+            Console.ResetColor();
+
+            return Results.Ok(new
+            {
+                mergedVideoUrl
+            });
         }
 
         FfmpegModel.ShowTrimVideos(trimsPath);
